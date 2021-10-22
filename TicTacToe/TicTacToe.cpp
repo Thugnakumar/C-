7eb6d5a54b8f69195e36c2f3199128a668cde4d8
Thugnakumar <375256@bsd48.org>
2021-10-22 12:11:48 -0700

    This is my TicTacToe game

--- TicTacToe/TicTacToe.cpp
+++ TicTacToe/TicTacToe.cpp
@@ -14,118 +14,186 @@ const int OMOVE = 2;
 //The following is a declaration of each function I use (besides main)
 void clearBoard(int(&board)[3][3]);
 void printBoard(int (&board)[3][3]);
-void playerMove(int (&board)[3][3]);
+void playerMove(int (&board)[3][3], int playerMove);
+bool checkWin(int (&board)[3][3], int player);
+bool boardFull(int (&board)[3][3]);
 
 int main() {
-  int board[3][3];
+  bool isPlaying = true; //checks if the user is still playing
+  int playerTurn = 1; //checks which player's turn (X or O) it is
+  int xWins = 0; //counts the number of times X has won
+  int oWins = 0;//counts the number of times O has won
+  int board[3][3]; //creates the board/int array
   clearBoard(board);
   printBoard(board);
-  playerMove(board);
+
+  while (isPlaying == true){
+    char playAgain = 'a'; //sets the isPlaying character to "a" so that it doesn't trigger the "y/n" function at the end
+    
+    playerMove(board, playerTurn); //activates playerMove() by passing in the board and the integer defining which player's turn it is
+    if (playerTurn == 1){
+      //switches the player's turn to O if it's currently X
+      playerTurn = 2;
+    }
+    else if (playerTurn == 2){
+      //switches the player's turn to X if it's currently O
+      playerTurn = 1;
+    }
+
+    if (boardFull(board) == true || checkWin(board, XMOVE) == true || checkWin(board, OMOVE) == true){
+      //the following code only activates if the board has no empty slots remaining (likely a tie) or if a player has won
+
+      if (checkWin(board, XMOVE) == true){
+	//checks if X has won, if X has won, then prints out the celebratory message and the number of times each player has won
+	cout << "Player X wins!" << endl;
+	xWins++;
+	cout << "X wins: " << xWins << " O wins: " << oWins << endl;
+      }
+
+      else if (checkWin(board, OMOVE) == true){
+	//checks if O has won, if O has won, then prints out the celebratory message and the number of times each player has won
+	cout << "Player O wins!" << endl;
+	oWins++;
+	cout << "X wins: " << xWins << " O wins: " << oWins << endl;
+      }
+      
+      else {
+	//if neither of the above statements are true, it is a tie and the according message prints out
+	cout << "Tie" << endl;
+	cout << "X wins: " << xWins << " O wins: " << oWins << endl;
+      }
+
+      while (playAgain != 'y' && playAgain != 'Y' && playAgain != 'n' && playAgain != 'N'){
+	//keeps asking the player to enter y or n for whether or not they want to play again until they enter y or n
+	cout << "Play again? (y/n) ";
+	cin >> playAgain;
+	cout << playAgain << endl;
+      }
+
+      if (playAgain == 'n' || playAgain == 'N'){
+	//terminates the program if the user doesn't want to play again
+	cout << "Goodbye!";
+	isPlaying = false;
+      }
+
+      else {
+	//resets the game if the user wants to play again
+	clearBoard(board);
+	printBoard(board);
+	playerTurn = 1;
+	continue;
+      }
+    }
+  }
   return 0;
 }
 
 void printBoard(int (&board)[3][3]){
+  //prints the board after each turn
 
-  board[0][1] = XMOVE;
-  cout << board[0][2] << endl;
-  cout << "\t1\t2\t3  "<< endl;
+  cout << "\t1\t2\t3  "<< endl; //the top of the grid
   for (int i = 0; i < 3; i++){
-    cout << char('a'+ i) << '\t';
+    cout << char('a'+ i) << '\t'; //the left end of grid
     for (int j = 0; j < 3; j++) {
-      //cout << board[i][j] << endl;
+      //iterates through each slot in the 2D array
       if (board[i][j] == XMOVE){
+	//if the specific slot in the 2D array is XMOVE, it prints an X in the corresponding part of the grid
 	cout << 'X';
       }
 
       else if (board[i][j] == OMOVE){
+	//if the specific slot in the 2D array is OMOVE, it prints an O in the corresponding part of the grid
 	cout << 'O';
       }
 
       else {
+	//if the specific slot in the 2D array is BLANK, it prints a space in the corresponding part of the grid
 	cout << ' ';
       }
 
       if (j < 2) {
+	//prints the bars between columns to make the grid
 	cout << char(board[i][j]) << "   " << '|' << "   ";
       }
 
       else {
+	//enters a carriage return at the end of the line
 	cout << char(board[i][j]) << '\n';
       }
       
     }
     if (i < 2) {
+      //prints out the dividers between each row
       cout << " " << '\t' << "----+-------+----" << '\n';
     }
   }
 }
-void playerMove(int (&board)[3][3]){
-  int playerMove = 1;
-  char row = 'f';
-  char column = '0';
-  char turn = 'X';
-  char realRow = 88;
-  char realColumn = 88;
-  bool openSlot = false;
+void playerMove(int (&board)[3][3], int playerMove){
+  //reads in where the player wants to place their marker, checks if it's a valid move, and places it on the board
+  char row = 'f';//this character keeps track of the current row
+  char column = '0';//this character keeps track of the current column
+  char realRow = 88;//this character finds the specific row in the array (uses ASCII)
+  char realColumn = 88;//this character finds the specific column in the array (uses ASCII)
+  bool openSlot = false;//registers whether or not a specific slot is open
 
   while (openSlot == false){
-    while (!(realRow > 47 && realRow < 51) && !(realColumn > 47 && realColumn < 51)) {
-      if (turn == 'X'){
-	cout << turn << " turn: ";
+    //keeps running until the user enters an input in a valid spot
+    while (!(realRow >= 0 && realRow <= 2) && !(realColumn >= 0 && realColumn <= 2)) {
+      //runs until the user inputs a valid row and column
+      if (playerMove == 1){
+	//prints "X turn: " if it's X's turn
+	cout << "X turn: ";
       }
 
-      else if (turn == 'O'){
-	cout << turn << " turn: ";
+      else if (playerMove == 2){
+	//prints "X turn: " if it's X's turn
+	cout << "O turn: ";
       }
       
-      cin >> row;
-      cin >> column;
+      cin >> row;//reads in user input for the row
+      cin >> column;//reads in user input for the column
+      cin.ignore(80,'\n');//removes any extra/unecessary characters
 
-      row = toupper(row);
+      row = toupper(row);//converts the column letter to an uppercase
 
-      realRow = row - 17;
-      realColumn = column - 1;
+      realRow = row - 65;//converts the row number to an ASCII value between 0 and 2
+      realColumn = column - 49;//converts the column number to an ASCII value between 0 and 2
 
-      if (!(realRow > 47 && realRow < 51) && !(realColumn > 47 && realRow < 51)) {
+      if (!(realRow >= 0 && realRow <= 2) && !(realColumn >= 0 && realRow <= 2)) {
+	//prints the following message if the input doesn't fall within the 0-2 boundary for row or column (after doing ASCII math)
 	cout << "Please enter 'a', 'b', or c' for the row and '1', '2', or '3' for the column. No spaces please!" << endl;
       }
     }
 
     if (board[realRow][realColumn] != BLANK){
+      //if there's already something in that specific array slot, print the following message and reset realRow and realColumn
       cout << "This spot is already taken!" << endl;
+      realRow = 88;
+      realColumn = 88;
       openSlot = false;
     }
 
     else {
+      //if the current space is a BLANK, then openSlot is true, allowing it to be filled in with an X or O
       openSlot = true;
     }
   }
 
-  cout << realRow << realColumn << endl;
-
-  if (turn == 'X'){
-    turn = 'O';
-  }
-
-  else if (turn == 'O'){
-    turn = 'X';
-  }
-  
   if (playerMove == 1){
+    //registers the user-specified slot as an XMOVE if it's X's turn
     board[int(realRow)][int(realColumn)] = XMOVE;
-    cout << "Xmove: " << board[int(realRow)][int(realColumn)] << endl;
-    playerMove = 2;
   }
 
   else {
+    //registers the user-specified slot as an OMOVE if it's O's turn
     board[int(realRow)][int(realColumn)] = OMOVE;
-    playerMove = 1;
   }
 
-  printBoard(board);
+  printBoard(board);//prints the board after each turn with the updated markers
 }
 
 void clearBoard(int (&board)[3][3]){
+  //goes through each index of the 2D array and sets the value to 0 (BLANK), thereby clearing the array of all markers
   for (int i = 0; i < 3; i ++) {
     for (int j = 0; j < 3; j++){
       board[i][j] = BLANK;
@@ -133,3 +201,24 @@ void clearBoard(int (&board)[3][3]){
   }
 }
 
+bool checkWin(int (&board)[3][3], int player){
+  //checks all the win conditions for the specific player passed in and checks to see if any of those win conditions are met. If they are, then the function returns true. Otherwise, it returns false.
+  if ((board[0][0] == player && board [0][1] == player && board[0][2] == player) || (board[1][0] == player && board[1][1] == player && board[1][2] == player) || (board[2][0] == player && board[2][1] == player && board[2][2] == player) || (board[0][0] == player && board[1][0] == player && board[2][0] == player) || (board[0][1] == player && board[1][1] == player && board[2][1] == player) || (board[0][2] == player && board[1][2] == player && board[2][2] == player) || (board[0][0] == player && board[1][1] == player && board[2][2] == player) || (board[0][2] == player && board[1][1] == player && board[2][0] == player)){
+    return true;
+  }
+
+  return false;
+}
+
+bool boardFull(int (&board)[3][3]){
+  //goes through each index of the board/array to see if any of them are spaces. If so, the program returns false. Otherwise, it returns true
+  for (int i = 0; i < 3; i++){
+    for (int j = 0; j < 3; j++){
+      if (board[i][j] == BLANK){
+	return false;
+      }
+    }
+  }
+
+  return true;
+}
