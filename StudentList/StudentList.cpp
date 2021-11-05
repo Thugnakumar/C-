f7594fe69c6466d06ffdb5ab2b1df1ca6a03b859
U-BSD\375256 <375256@SHS-10D9C06CC.beaverton.k12.or.us>
2021-11-05 12:15:08 -0700

    This is the student list project

--- StudentList/StudentList.cpp
+++ StudentList/StudentList.cpp
@@ -7,67 +7,63 @@ This program is titled Student List. It allows the user to do one of 4 things: a
 #include <cstring>
 #include <vector>
 #include <cstring>
+#include <math.h>
 
 using namespace std;
 
 struct student {
+  //the blueprint for creating a new student struct
   char firstName[20];
   char lastName[20];
   int id;
   float gpa;
 };
 
-<<<<<<< HEAD
-
+//function declarations
 void add(vector <student*> &v);
 void remove(vector <student*> &v);
 void print(vector <student*> &v);
 
 int main() {
-  vector <student*> list;
-  char input[10];
-=======
-void add(vector <student*> &v);
-void remove(vector <student*> &v);
-void printList(vector <student*> &v);
-
-int main() {
-  vector <student*> list;
-  char input [10];
->>>>>>> f817feba904852d7f2062e5a1a7c3ba860e0f7e3
-  while (true) {
-    cout << "Please enter a command: ";
-    cin >> input;
+  //where all the action happens
+  vector <student*> list;//creates a new list of student pointers
+  char input[10];//reads in the user's input to determine what command they give
+  while (true) {//keeps running forever
+    cout << "Please enter a command: "; //prompts the user for a command
+    cin >> input;//reads in the command
     for (int i = 0; i < strlen(input); i++){
+      //changes the user's input to purely uppercase characters
       input[i] = toupper(input[i]);
     }
 
     if (strcmp(input, "ADD") == 0){
+      //resorts to the add function if the user types "add"
       add(list);
     }
 
     else if (strcmp(input, "DELETE") == 0){
+      //refers to the remove function if the user types "remove"
       remove(list);
     }
 
+    else if (strcmp(input, "CLEAR") == 0){
+      //clears the vector if the user types "clear"
+      list.clear();
+    }
+
     else if (strcmp(input, "PRINT") == 0){
-<<<<<<< HEAD
+      //refers to the print function if the user types print
       print(list);
-=======
-      printList(list);
->>>>>>> f817feba904852d7f2062e5a1a7c3ba860e0f7e3
     }
 
     else if (strcmp(input, "QUIT") == 0){
+      //exits the program if the user types quit
       break;
     }
 
     else {
-<<<<<<< HEAD
+      //if none of the above inputs are the same as what the user typed in, then it's an invalid command and the following message prints on the screen
       cout << "Not a valid command! Please type 'ADD' to add a new student to the list, 'DELETE' to remove a student from the list, 'PRINT' to print all the students in the list, or 'QUIT' to exit the program" << endl;
-=======
-      cout << "Not a valid command!" << endl;
->>>>>>> f817feba904852d7f2062e5a1a7c3ba860e0f7e3
     }
   }
 
@@ -76,7 +72,7 @@ int main() {
 }
 
 void add(vector <student*> &v){
-<<<<<<< HEAD
+  //passes the vector in by reference and prompts the user to input all of the parameters that make up a student (as defined by the struct at the top)
   student* newStudent = new student();
   cout << "Enter the student's first name: ";
   cin >> newStudent -> firstName;
@@ -86,59 +82,54 @@ void add(vector <student*> &v){
   cin >> newStudent -> id;
   cout << endl << "Enter the student's GPA: ";
   cin >> newStudent -> gpa;
-  v.push_back(newStudent);
+  newStudent -> gpa = round(newStudent -> gpa * 100)/100;
+  v.push_back(newStudent); //actually pushes out the newly created student to the vector/list
+  cout << "New student added!" << endl; //prints a confirmation message when a student is successfully added
 }
 
 void remove(vector <student*> &v){
+  //removes a particular student based on what ID the user types in
   int studentID = -1;
-  cout << "Please type the ID of the student you wish to delete ";
-  print(v);
-  cin >> studentID;
-  for (vector <student*> :: iterator it = v.begin(); it != v.end(); ++it){
-    if (studentID == (*it) -> id){
-      v.erase(it);
-      delete *it;
-      break;
+  if (!v.empty()){
+    //the program first checks to see if there is an actual student in the list first, before it deletes anything
+    cout << "Please type the ID of the student you wish to delete. Here are the current students in your list:" << endl; //prompts the user to type the ID of the student they wish to delete
+    print(v); //prints out the list of current students so that the user can reference what students they have and their properties before deleting
+    cin >> studentID; //reads in the user's input for ID
+    for (vector <student*> :: iterator it = v.begin(); it != v.end(); ++it){
+      //iterates through each element in the vector
+      if (studentID == (*it) -> id){
+	//compares the user input ID and the ID of each student until they match, then removes that student from the vector and deletes it completely
+	v.erase(it);
+	delete *it;
+	cout << "Deleted!" << endl;
+	break;
+      }
+
+      else {
+	//if the ID doesn't match with any of the IDs of the students, the following message prints
+	cout << "Not a valid ID!" << endl;
+      }
     }
+  }
+
+  else {
+    //if there's nothing in the list, the following message prints
+    cout << "There is nothing in your list to delete!" << endl;
   }
 }
 
 void print(vector <student*> &v){
-  //the following code was found on https://www.cplusplus.com/reference/vector/vector/begin/
-  for (vector <student*> :: iterator it = v.begin(); it != v.end(); ++it){
-    cout << "First Name: " << (*it) -> firstName << " | Last Name: " << (*it) -> lastName << " | ID: " << (*it) -> id << " | GPA: " << (*it) -> gpa << endl;
-  }
-=======
-  student* newStudent;
-  cout << "Enter the student's first name: ";
-  cin >> newStudent->firstName;
-  cout << endl << "Enter the student's last name: ";
-  cin >> newStudent->lastName;
-  cout << endl << "Enter the student's ID number: ";
-  cin >> newStudent->id;
-  cout << endl << "Enter the student's GPA: ";
-  cin >> newStudent->gpa;
-  cout << endl;
-  v.push_back(newStudent); //I found this using https://www.cplusplus.com/reference/vector/vector/push_back/
-  
-}
-
-void remove(vector <student*> &v){
-  int input = -32;
-  cout << "Please enter the ID number of the student you wish to delete from the list: ";
-  cin >> input;
-  //if (student
-}
-
-void print(vector <student*> &v){
-  cout << student->name;
-}
-    
-  /*void clearInput((&input)char[10]){
-  for (int i = 0; i < 10; i++){
-    if (input[i] != '\0'){
-      input[i] = 0;
+  //this function prints all of the students in the vector, along with their properties
+  if (!v.empty()){
+    //makes sure that the vector isn't empty first
+    for (vector <student*> :: iterator it = v.begin(); it != v.end(); ++it){
+      //iterates through each element in the vector and prints each of the students' properties
+      cout << "First Name: " << (*it) -> firstName << " | Last Name: " << (*it) -> lastName << " | ID: " << (*it) -> id << " | GPA: " << (*it) -> gpa << endl;
     }
   }
-  }*/
->>>>>>> f817feba904852d7f2062e5a1a7c3ba860e0f7e3
+
+  else {
+    //if the list is empty, the following message prints
+    cout << "There is nothing in your list to print!" << endl;
+  }
+}
