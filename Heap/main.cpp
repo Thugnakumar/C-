26050902ddb455a801c318c1dca23070eeb867a3
U-BSD\375256 <375256@SHS-10DCE0874.beaverton.k12.or.us>
2022-03-29 14:26:53 -0700

    HEEEEEEEAAAAAAP

--- Heap/main.cpp
+++ Heap/main.cpp
@@ -10,19 +10,12 @@ This is the heap program! It takes in input from either a file or through user i
 
 using namespace std;
 
-struct bnode {
-  bnode * right;
-  bnode * left;
-  int data;
-};
-
-void createAndAdd(int number);
-
 int main() {
-  int input = 0;
+  int input1 = 0;
+  int input2 = 0;
   char arr[2];
   int inputNums[100];
-  //  int heap[];
+  int heap[100];
 
   for (int i = 0; i < 100; ++i) {
     inputNums[i] = -1;
@@ -30,35 +23,19 @@ int main() {
   
   cout << "Would you like to input numbers manually (M) or using a file (F)? ";
   cin >> arr;
-  if (arr == "M" || arr == "m") {
-    cout << "How many numbers would you like to input?";
-    cin >> input;
-    for (int i = 0; i < input; ++i) {
+  if (strcmp(arr, "M") == 0 || strcmp(arr, "m") == 0) {
+    cout << "How many numbers would you like to input? ";
+    cin >> input1;
+    for (int i = 0; i < input1; ++i) {
       cout << "Enter a number: ";
-      cin >> input;
+      cin >> input2;
       for (int i = 0; i < 100; ++i) {
 	if (inputNums[i] == -1) {
-	  inputNums[i] = input;
+	  inputNums[i] = input2;
+	  cout << inputNums[i] << endl;
 	  break;
 	}
       }
     }
-    for (int i = 0; i < 100; ++i) {
-      if (inputNums[i] != -1) {
-	cout << inputNums[i];
-      }
-    }
   }
-  while (true) {
-    cout << "Enter a number: ";
-    cin >> input;
-    createAndAdd(input);
-  }
-}
-
-void createAndAdd(int number) {
-  bnode * newBnode = new bnode();
-  newBnode->right = NULL;
-  newBnode->left = NULL;
-  newBnode->data = number;
 }
